c87c4f4dada32dff740174b5823b8ae7af8f9e01
sarah <saraharkam95@gmail.com>
2018-10-24 14:45:42 +0200

    Initial commit

---
+++ Chronomètre/app/src/main/java/com/example/sarah/chronometre/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.sarah.chronometre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.os.SystemClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button Start;
+    private Button Stop;
+    private Button Reset;
+    private Chronometer Chrono;
+    private long dureePause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Chrono= (Chronometer)findViewById(R.id.chronometre_id);
+        Start= (Button)findViewById(R.id.start_id);
+        Stop= (Button)findViewById(R.id.stop_id);
+        Reset= (Button)findViewById(R.id.reset_id);
+    }
+
+    public void startClick(View view) {//à rechercher
+
+
+
+        if (dureePause==0) {
+            Chrono.setBase(SystemClock.elapsedRealtime());
+
+        }
+
+          else if(dureePause != 0) {
+
+            Chrono.setBase(SystemClock.elapsedRealtime()+ Chrono.getBase()- dureePause);
+        }
+
+        Chrono.start();
+    }
+
+
+    public void stopClick(View view){
+
+        Chrono.stop();
+        dureePause= SystemClock.elapsedRealtime();
+
+    }
+
+
+    public void resetClick(View view){
+
+        Chrono.stop();
+        dureePause = 0;
+        Chrono.setText("0");
+        Chrono.setBase(SystemClock.elapsedRealtime());
+        Chrono.start();
+
+
+
+
+    }
+}
+
